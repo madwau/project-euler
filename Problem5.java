2eaa1935ac43cf72e61d289a0da73fccfa2d555a
madwau <martin.wauligmann@gmail.com>
2014-08-06 01:14:37 +0200

    Problem: 5

---
+++ Problem5.java
@@ -0,0 +1,25 @@
+public class Problem5
+{
+	public static void main(String[] args)
+	{
+		boolean found = false;
+		int min = 20;
+		
+		while(!found)
+		{
+			found = true;
+			
+			for(int i = 2; i <= 20; i++)
+			{
+				if(min % i != 0)
+				{
+					found = false;
+					min++;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(min);
+	}
+}
