7e2a28ec041f43ed935c5800a05f7007c2ecd741
madwau <martin.wauligmann@gmail.com>
2014-08-06 23:47:43 +0200

    Problem: 7

---
+++ Problem7.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem7
+{
+	public static void main(String[] args)
+	{
+		List<Integer> primes = new ArrayList<Integer>();
+		primes.add(2);
+		
+		int count = 1;
+		int candidate = 3;
+		
+		while(count < 10001)
+		{
+			boolean prime = true;
+			
+			for(int i : primes)
+			{
+				if(candidate % i == 0)
+				{
+					prime = false;
+					break;
+				}
+			}
+			
+			if(prime)
+			{
+				primes.add(candidate);
+				count++;
+			}
+			
+			candidate++;
+		}
+		
+		System.out.println(candidate-1);
+	}
+}
