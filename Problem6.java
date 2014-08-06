1924e227e408e6771d4789cd68e6553235f35651
madwau <martin.wauligmann@gmail.com>
2014-08-06 23:30:26 +0200

    Problem: 6

---
+++ Problem6.java
@@ -0,0 +1,11 @@
+public class Problem6
+{
+	public static void main(String[] args)
+	{
+		int n = 100;
+		int sum_of_squares = n*(n+1)*(2*n+1)/6;
+		int square_of_sum = n*(n+1)*n*(n+1)/4;
+		
+		System.out.println(square_of_sum-sum_of_squares);
+	}
+}
