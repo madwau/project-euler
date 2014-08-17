f68ac9c0bdc8dd589f50465748e7be1496d1ba97
madwau <martin.wauligmann@gmail.com>
2014-08-17 20:39:06 +0200

    Problem 17

---
+++ Problem017.java
@@ -0,0 +1,13 @@
+public class Problem017
+{
+	public static void main(String[] args)
+	{
+		String num1 = "onetwothreefourfivesixseveneightnine";
+		String num2 = "teneleventwelvethirteenfourteenfifteensixteenseventeeneighteennineteen";
+		String num3 = "twentythirtyfortyfiftysixtyseventyeightyninety";
+		String num4 = "hundredand";
+		String num5 = "onethousand";
+		
+		System.out.println(10*(9*num1.length()+10*num3.length()+num2.length())+100*num1.length()+900*num4.length()-9*3+num5.length());
+	}
+}
