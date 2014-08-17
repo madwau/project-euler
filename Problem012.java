088383106243b7c6adf6ad50be0cf1239aade7f3
madwau <martin.wauligmann@gmail.com>
2014-08-17 17:51:54 +0200

    Problem 12

---
+++ Problem012.java
@@ -0,0 +1,19 @@
+public class Problem012
+{
+	public static void main(String[] args)
+	{
+		for(int i = 3, triangle = 3, count = 0; ; triangle += i++, count = 0)
+		{
+			for(int j = 1; j*j <= triangle; j++)
+			{
+				if(triangle % j == 0) count += 2;
+			}
+			
+			if(count > 500)
+			{
+				System.out.println(triangle);
+				break;
+			}
+		}
+	}
+}
