6b8f2fdf9845e34232924f7db333624b25d115a2
madwau <martin.wauligmann@gmail.com>
2014-08-17 19:09:45 +0200

    Problem 14

---
+++ Problem014.java
@@ -0,0 +1,29 @@
+public class Problem014
+{
+	public static void main(String[] args)
+	{
+		int num = 0;
+		int max = 0;
+		
+		for(int n = 1; n < 1000000; n++)
+		{
+			long t = n;
+			int length = 1;
+			
+			while(t != 1)
+			{
+				if(t % 2 == 0) t = t/2;
+				else t = 3*t + 1;
+				length++;
+			}
+			
+			if(length > max)
+			{
+				max = length;
+				num = n;
+			}
+		}
+		
+		System.out.println(num);
+	}
+}
