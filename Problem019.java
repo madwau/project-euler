376106f60b7682c0933e09e69021a8a303171e81
madwau <martin.wauligmann@gmail.com>
2014-08-18 17:52:29 +0200

    Problem 19

---
+++ Problem019.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+
+public class Problem019
+{
+	public static void main(String[] args)
+	{
+		Calendar c = Calendar.getInstance(); 
+		c.set(1901, 1, 1);
+		
+		int count = 0;
+		
+		while(c.get(Calendar.YEAR) < 2001) 
+		{
+			if(c.get(Calendar.DAY_OF_MONTH) == 1 &&
+			   c.get(Calendar.DAY_OF_WEEK)  == Calendar.SUNDAY)
+			{
+				count++;
+			}
+			
+			c.add(Calendar.DATE, 1);
+		}
+		
+		System.out.println(count);
+	}
+}
