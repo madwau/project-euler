548d4ce857292de193691197f5fd92c00d23d9d2
madwau <martin.wauligmann@gmail.com>
2014-08-19 19:18:02 +0200

    Problem 31

--- Problem031.java
+++ Problem031.java
@@ -9,17 +9,16 @@ public class Problem031
 		System.out.println(num(0, 200, new ArrayList<Integer>(Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200))));
 	}
 	
-	static int num(int sum, int max, List<Integer> remainingCoins)
+	static int num(int sum, int max, List<Integer> coins)
 	{
 		// Target value has been matched: 1 solution found
 		if(sum == max) return 1;
 		
 		// Target value cannot be matched anymore: 0 solutions found
-		if(sum > max || remainingCoins.size() == 0) return 0;
+		if(sum > max || coins.size() == 0) return 0;
 		
 		// 1st: Include current coin and leave remainingCoins unaltered
 		// 2nd: Stop including current coin and move to next one
-		return num(sum + remainingCoins.get(0), max, remainingCoins) +
-		       num(sum, max, remainingCoins.subList(1, remainingCoins.size()));
+		return num(sum + coins.get(0), max, coins) + num(sum, max, coins.subList(1, coins.size()));
 	}
 }
