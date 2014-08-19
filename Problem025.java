407b2bd2609ad9bd2bf2e0ab8d5bbe11d9ea0e0d
madwau <martin.wauligmann@gmail.com>
2014-08-19 16:20:30 +0200

    Problem 25

---
+++ Problem025.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+public class Problem025
+{
+	public static void main(String[] args)
+	{
+		int count = 2;
+		
+		BigInteger f = BigInteger.ONE, g = f;
+		
+		while(String.valueOf(g).length() < 1000) 
+		{
+			g = g.add(f);
+			f = g.subtract(f);
+			count++;
+		}
+		
+		System.out.println(count);
+	}
+}
